354d51062388a839d5858bd17d7c365e1dc4a54d
Fynn Muermanns <fynn.m@live.de>
2022-08-09 14:58:37 +0200

    More work on the GUI implementation

--- src/fastagen/GenPaneController.java
+++ src/fastagen/GenPaneController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.TextField;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
@@ -16,7 +17,7 @@ public class GenPaneController implements Initializable
 {
 
     @FXML
-    Button gPdone, gPback;
+    Button gPdone, gPback, gPsaveAs, gPstartCalc;
 
     @FXML
     ChoiceBox<String> gPalphabetBox;
@@ -24,12 +25,28 @@ public class GenPaneController implements Initializable
     @FXML
     ChoiceBox<Integer> gPthreadsBox, gPseqsBox;
 
+    @FXML
+    TextField gPseqLengthFrom, gPseqLengthTo, gPseqs, gPfastaHeaderPrefix;
+
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle)
+    public void initialize(URL url, ResourceBundle rb)
     {
         gPalphabetBox.setItems(FXCollections.observableArrayList("Genome", "Protein"));
-        gPthreadsBox.setItems(FXCollections.observableArrayList(1, 2, 4, 8));
+        gPalphabetBox.setValue("Genome");
+        
+        int maxThreads = Runtime.getRuntime().availableProcessors();
+        int defaultThreads = (int) (maxThreads * 0.75);
+        gPthreadsBox.getItems().add(1);
+        for (int i=2; i<maxThreads+2; i += 2) gPthreadsBox.getItems().add(i);
+        gPthreadsBox.setValue(defaultThreads);
+
         gPseqsBox.setItems(FXCollections.observableArrayList(1, 5, 10, 50));
+        gPseqsBox.setValue(1);
+
+        gPsaveAs.setDisable(true);
+        gPdone.setVisible(false);
+
+
     }
 
     @FXML
@@ -43,6 +60,15 @@ public class GenPaneController implements Initializable
     public void back() throws Exception
     {
         switchPane(gPback, "startPane.fxml");
+    }
+
+    @FXML
+    public void startCalc()
+    {
+        // generate the Fasta entries and start the calculations
+        gPstartCalc.setVisible(false);
+        gPdone.setVisible(true);
+        gPsaveAs.setDisable(false);
     }
 
     private void switchPane(Button b, String fxmlFile) throws Exception
--- src/fastagen/ReadPaneController.java
+++ src/fastagen/ReadPaneController.java
@@ -4,16 +4,24 @@ import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.TextField;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
 
-public class ReadPaneController
+
+public class ReadPaneController implements Initializable
 {
 
     @FXML
-    Button rPdone, rPback;
+    Button rPdone, rPback, rPreadFiles, rPopenDialog, rPplot;
+
+    @FXML
+    ChoiceBox<String> rPmodeBox;
 
     @FXML
     public void close()
@@ -26,6 +34,14 @@ public class ReadPaneController
     public void back() throws Exception
     {
         switchPane(rPback, "startPane.fxml");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+        rPmodeBox.setItems(FXCollections.observableArrayList("Genome", "Protein"));
+        rPmodeBox.setValue("Genome");
+
     }
 
     private void switchPane(Button b, String fxmlFile) throws Exception
